e8ae6592a3dbcd2308373a7a3990078cf97a6e36
Viraj-Lakshitha12 <164119893+Viraj-Intelleon@users.noreply.github.com>
2024-07-08 21:33:11 +0530

    create login page

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 import '../index.css';
 
+interface UserData {
+  username: string;
+  password: string;
+}
+
 export default function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async () => {
+    const userData: UserData = { username: username, password: password };
+    try {
+      const response = await axios.post('http://localhost:8080/auth/login', userData);
+      // Handle successful login (e.g., store token, redirect)
+      console.log('Login successful', response.data);
+    } catch (error) {
+      // Handle error (e.g., show error message)
+      console.error('Login failed', error);
+      setError('Invalid username or password');
+    }
+  };
+
   return (
-    <h1 className="text-3xl m-48  font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex h-screen bg-gradient-custom">
+      <div className="w-1/2 flex justify-center items-center">
+        <img
+          src="src/assets/images/Group 160.png"
+          alt="main image"
+          className="rounded-right w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-1/2 flex flex-col justify-center items-center">
+        <div className="mb-8">
+          <img
+            src="src/assets/images/logo.png"
+            alt="logo"
+            className="w-[15vw] h-[30vh] object-contain"
+          />
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <input
+            placeholder="user name"
+            className="p-2 rounded-md border border-gray-400 w-[30vw] m-3"
+            value={username}
+            onChange={(ev) => setUsername(ev.target.value)}
+          />
+          <input
+            placeholder="password"
+            type="password"
+            className="p-2 rounded-md border border-gray-400 w-[30vw] m-3"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+          {error && <div className="text-red-500">{error}</div>}
+          <button type="button" onClick={handleLogin} className="custom-border-colour w-[10vw] h-[5vh]">
+            <span className="custom-font-colour">Sign In</span>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
