535e2acdac692e51fbce9004c1a4444bff7cdf33
Shivantha56 <shiwanthadilshan3@gmail.com>
2024-07-25 12:35:29 +0530

    add new customer model

--- src/components/order/order.tsx
+++ src/components/order/order.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import TopNavbar from '../topNavbar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    color: "white",
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "fit-content",
+    bgcolor: '#14141E',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+    borderRadius: "10px"
+};
+
 
 export default function Order() {
+
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
     return (
         <div className='m-4 w-full'>
             <div className="m-4">
@@ -12,7 +37,7 @@ export default function Order() {
                 <div className='flex items-center justify-between'>
                     <div className='flex items-center gap-10'>
                         <p className='font-bold text-[#5386ED] text-3xl'>#00000253</p>
-                        <button className='border-2 border-[#5386ED] text-white rounded-full py-1 px-3'>Add Customer</button>
+                        <button onClick={handleOpen} className='border-2 border-[#5386ED] text-white rounded-full py-1 px-3'>Add Customer</button>
                         <button className='border-2 border-[#5386ED] text-white rounded-full py-1 px-3'>Add Phone</button>
                         <button className='border-2 border-[#5386ED] text-white rounded-full py-1 px-3' >Add Item</button>
                     </div>
@@ -25,16 +50,16 @@ export default function Order() {
                 </div>
             </div>
 
-            <div className='flex justify-between mt-5'>
-                <div className='flex-1'>
+            <div className='flex justify-between mt-5 items-center'>
+                <div className='flex items-center'>
                     <input
                         className='w-full text-feild mb-4 md:mb-0 md:w-[30%] lg:mx-2 md:mx-2 sm:mx-1'
                         value={""}
                         // onChange={(ev) => setModel(ev.target.value)}
                         placeholder='   Contact Number'
                     />
-                    <button>
-
+                    <button className='flex flex-col items-center h-full text-field border-1 border-[#5386ED] text-white mb-4 md:mb-0 '>
+                        <img src="src/assets/icons/search-alt-2-svgrepo-com 1.svg" alt="" />
                     </button>
                 </div>
                 <div>
@@ -76,13 +101,61 @@ export default function Order() {
                                 <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{item.price}</td>
                             </tr>
                         ))} */}
-                    </tbody> }
+                    </tbody>}
                 </table>
             </div>
             <div className='w-full flex gap-2 mt-5 justify-end'>
                 <button className='bg-[#00900F] p-1 rounded-md text-white font-bold'>Proceed To Payment</button>
                 <button className='bg-[#B10000] p-1 rounded-md text-white font-bold'>Cancel Payment</button>
             </div>
+
+            {/* models */}
+            <div>
+                {/* <Button onClick={handleOpen}>Open modal</Button> */}
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Typography className='text-center' id="modal-modal-title" variant="h5" component="h2">
+                            Add new customer
+                        </Typography>
+                        <div className='w-full flex flex-col items-center mt-2'>
+                            <input
+                                className='text-feild mb-4 md:mb-0 md:w-[30%] lg:mx-2 md:mx-2 sm:mx-1'
+                                value={""}
+                                // onChange={(ev) => setModel(ev.target.value)}
+                                placeholder='   Contact Name'
+                            />
+                            <input
+                                className='text-feild mb-4 md:mb-0 md:w-[30%] lg:mx-2 md:mx-2 sm:mx-1'
+                                value={""}
+                                // onChange={(ev) => setModel(ev.target.value)}
+                                placeholder='   Email'
+                            />
+                            <input
+                                className='text-feild mb-4 md:mb-0 md:w-[30%] lg:mx-2 md:mx-2 sm:mx-1'
+                                value={""}
+                                // onChange={(ev) => setModel(ev.target.value)}
+                                placeholder='   Whatsapp Number'
+                            />
+                            <input
+                                className='text-feild mb-4 md:mb-0 md:w-[30%] lg:mx-2 md:mx-2 sm:mx-1'
+                                value={""}
+                                // onChange={(ev) => setModel(ev.target.value)}
+                                placeholder='   Nic'
+                            />
+                        </div>
+                        <div className='w-full flex gap-2 mt-5 justify-center'>
+                            <button className='bg-[#00900F] p-2 rounded-md text-white font-bold'>Save customer</button>
+                            <button className='bg-[#B10000] p-2 rounded-md text-white font-bold'>Cancel</button>
+                        </div>
+                    </Box>
+                </Modal>
+            </div>
+
         </div>
     );
 }
