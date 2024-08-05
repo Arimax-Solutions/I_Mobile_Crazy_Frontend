e2b910984bbda114def54d7aa234f9b3591e7332
ChanukaWeerakkody <chanuka.weerakkody123@gmail.com>
2024-08-06 01:27:51 +0530

    update

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -6,7 +6,21 @@ function Layout({ children }: any) {
     return (
         <div className="flex min-h-screen bg-[#14141E]">
             <NavigationMenu />
-            <Outlet />
+            <main
+                style={{
+                    flex: 1,
+                    padding: '20px 300px',
+                    overflow: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'flex-start',
+                }}
+                className="flex-1 overflow-auto"
+            >
+                <div style={{ width: '1750px', maxWidth: '1750px' }}>
+                    <Outlet />
+                </div>
+            </main>
         </div>
 
     )
--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { clsx } from 'clsx';
+import { Link, NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
 function NavigationMenu() {
-
-
-    //if the nav item name has the two words like [Stock phones] the below state value is changes as [Stock-phones] 
     const [select, setSelect] = useState<string>("Reports");
 
-
     return (
-        <nav className="pt-3 flex flex-col min-w-[270px] w-[270px] min-h-screen h-fit bg-[#1D1D27] rounded-r-[40px] items-center">
-            <div className='mt-2 self-start pl-8'>
-                <img width={"100px"} src="src/assets/images/logo.png" alt="company logo" />
+        <nav className="fixed top-10 left-0 pt-3 flex flex-col min-w-[270px] w-[270px] h-[90%] bg-[#1D1D27] rounded-r-[40px] items-center overflow-hidden">
+            <div className='mt-2 align-self-md-start pl-8'>
+                <img width={"120px"} src="src/assets/images/logo.png" alt="company logo" />
             </div>
-            <div className="overflow-y-auto w-full self-start mb-4">
-                <ul className='mt-5 w-full pl-8 flex flex-col gap-10 mb-2 '>
+            <div className="w-full h-full flex flex-col">
+                <div className="w-full flex-1 overflow-y-auto">
+                    <ul className='mt-10 w-full pl-8 flex flex-col gap-10 mb-2'>
+                        <NavItems logo='src/assets/images/navbar/layer1.svg' name='Reports' location='/dashboard' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Stock Phones.svg' name='Stock Phones' location='/StockPhones' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Return Phones.svg' name='Return Phones' location='/returnPhone' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Items.svg' name='Items' location='/item' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Return item.svg' name='Return Items' location='/returnItem' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Users.svg' name='Users' location='/user' selected={setSelect} isSelected={select} />
+                        <NavItems logo='src/assets/images/navbar/Shops.svg' name='Shops' location='/shop' selected={setSelect} isSelected={select} />
+                        {/*<NavItems logo='src/assets/images/navbar/Shops.svg' name='order' location='/order' selected={setSelect} isSelected={select} />*/}
+                    </ul>
+                </div>
 
-                    <NavItems logo='src/assets/images/navbar/layer1.svg' name='Reports' location='/dashboard' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Stock Phones.svg' name='Stock Phones' location='/StockPhones' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Return Phones.svg' name='Return Phones' location='/returnPhone' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Items.svg' name='Items' location='/item' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Return item.svg' name='Return Items' location='/returnItem' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Users.svg' name='Users' location='/user' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Shops.svg' name='Shops' location='/shop' selected={setSelect} isSelected={select} />
-                    <NavItems logo='src/assets/images/navbar/Shops.svg' name='order' location='/order' selected={setSelect} isSelected={select} />
-
-                </ul>
                 <div className='w-full px-8 mb-5'>
-                    <button type="submit" className="mt-5 flex text-[25px] w-full justify-center rounded-md bg-[#5356EC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 items-center gap-3"> <img src="src/assets/images/navbar/cart-svgrepo-com 1.svg" alt="" />Place Order</button>
+                    <NavLink to="/order" className="mt-5 flex text-[25px] w-full justify-center rounded-md bg-[#5356EC] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 items-center gap-3">
+                        <img src="src/assets/images/navbar/cart-svgrepo-com 1.svg" alt="" />
+                        Place Order
+                    </NavLink>
                 </div>
             </div>
+
         </nav>
-    )
+    );
 }
 
 export default NavigationMenu;
 
-// navigation items component
 interface INavItems {
     name: string
     location: string
