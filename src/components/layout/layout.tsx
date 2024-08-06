5979c965a9cb3adfce6bc5d5f74d4d1553a3dc4c
ChanukaWeerakkody <chanuka.weerakkody123@gmail.com>
2024-08-06 17:39:48 +0530

    update layout margin

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,16 +5,13 @@ import { Outlet } from 'react-router-dom';
 function Layout({ children }: any) {
     return (
         <div className="flex min-h-screen bg-[#14141E]">
-            <div className="w-[10%]">
+            <div className="w-full md:w-[25vw] lg:w-[20vw] xl:w-[15vw]">
                 <NavigationMenu />
             </div>
-            <main
-                className="w-[90%] flex-1 p-8 overflow-y-auto flex flex-col justify-start"
-            >
-                <div className="w-full max-w-full overflow-x-hidden">
+            <main className="w-full max-w-[85%] flex-1 p-8 overflow-y-auto flex flex-col justify-start mx-auto">
+                <div className="w-full overflow-x-hidden">
                     <Outlet />
                 </div>
-
             </main>
         </div>
     );
