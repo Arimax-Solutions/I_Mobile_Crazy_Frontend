ff9e3b0ce90a2f945fe53bb352f30cf4574476e5
Viraj Lakshitha <viraj.lakshitha.22222@gmail.com>
2024-08-20 17:31:55 +0530

    change user validation part

--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -273,14 +273,17 @@ const User: React.FC = () => {
             <div className="m-4">
                 <TopNavbar />
             </div>
-
+    
             <div className='m-4 text-white font-semibold'>
                 <div className='mt-5 flex justify-between'>
                     <div className='flex flex-col'>
                         <input
                             className='text-feild'
                             value={name}
-                            onChange={(event) => setName(event.target.value)}
+                            onChange={(event) => {
+                                setName(event.target.value);
+                                setNameError(''); // Clear the name error when typing
+                            }}
                             placeholder='   Name'
                         />
                         {nameError && <span className='text-red-500 text-xs ml-[0.5vw]'>{nameError}</span>}
@@ -289,7 +292,10 @@ const User: React.FC = () => {
                         <select
                             className='text-feild'
                             value={role}
-                            onChange={(event) => setRole(event.target.value)}
+                            onChange={(event) => {
+                                setRole(event.target.value);
+                                setRoleError(''); // Clear the role error when typing
+                            }}
                         >
                             <option value="">Role</option>
                             <option value="user">User</option>
@@ -300,19 +306,25 @@ const User: React.FC = () => {
                         <input
                             className='text-feild'
                             value={contact_number}
-                            onChange={(event) => setContactNumber(event.target.value)}
+                            onChange={(event) => {
+                                setContactNumber(event.target.value);
+                                setContactNumberError(''); // Clear the contact number error when typing
+                            }}
                             placeholder='   Contact Number'
                         />
                         {contactNumberError && <span className='text-red-500 text-xs ml-[0.5vw]'>{contactNumberError}</span>}
                     </div>
                 </div>
-
+    
                 <div className='mt-4 flex justify-between'>
                     <div className='flex flex-col'>
                         <input
                             className='text-feild'
                             value={email}
-                            onChange={(event) => setEmail(event.target.value)}
+                            onChange={(event) => {
+                                setEmail(event.target.value);
+                                setEmailError(''); // Clear the email error when typing
+                            }}
                             placeholder='   Email'
                         />
                         {emailError && <span className='text-red-500 text-xs ml-[0.5vw]'>{emailError}</span>}
@@ -321,7 +333,10 @@ const User: React.FC = () => {
                         <input
                             className='text-feild'
                             value={username}
-                            onChange={(event) => setUsername(event.target.value)}
+                            onChange={(event) => {
+                                setUsername(event.target.value);
+                                setUsernameError(''); // Clear the username error when typing
+                            }}
                             placeholder='   Username'
                         />
                         {usernameError && <span className='text-red-500 text-xs ml-[0.5vw]'>{usernameError}</span>}
@@ -330,14 +345,17 @@ const User: React.FC = () => {
                         <input
                             className='text-feild'
                             value={password}
-                            onChange={(event) => setPassword(event.target.value)}
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                                setPasswordError(''); // Clear the password error when typing
+                            }}
                             placeholder='   Password'
                         />
                         {passwordError && <span className='text-red-500 text-xs ml-[0.5vw]'>{passwordError}</span>}
                     </div>
                 </div>
             </div>
-
+    
             <div className='m-4 flex mt-5 justify-end'>
                 <Button
                     onClick={() => handleAddUser()}
@@ -364,39 +382,38 @@ const User: React.FC = () => {
                     UPDATE
                 </Button>
             </div>
-
+    
             {/* Table to display users */}
             <div className='mt-5 m-4'>
                 <table className='min-w-full divide-y table-styles'>
                     <thead>
-                    <tr>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Id</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Name</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Contact Number</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Email</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Username</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Password</th>
-                        <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Role</th>
-                    </tr>
+                        <tr>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Id</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Name</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Contact Number</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Email</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Username</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Password</th>
+                            <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>Role</th>
+                        </tr>
                     </thead>
                     <tbody className='divide-y divide-gray-200'>
-                    {users.map((user) => (
-                        <tr key={user.user_id} onClick={() => handleTableRowClick(user)} className='text-white font-semibold hover:bg-gray-50 cursor-pointer'>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm font-medium text-gray-500'>{user.user_id}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.name}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.contact_number}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.email}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.username}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.password}</td>
-                            <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.role}</td>
-                        </tr>
-                    ))}
+                        {users.map((user) => (
+                            <tr key={user.user_id} onClick={() => handleTableRowClick(user)} className='text-white font-semibold hover:bg-gray-50 cursor-pointer'>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm font-medium text-gray-500'>{user.user_id}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.name}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.contact_number}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.email}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.username}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.password}</td>
+                                <td className='px-6 py-2 whitespace-nowrap text-sm text-gray-500'>{user.role}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
-
         </div>
     );
-};
+}    
 
 export default User;
